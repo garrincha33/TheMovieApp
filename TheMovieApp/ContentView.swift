376ce91a9436e28c84f4b1c6e8d523cb470efdcc
Richard Price <mrspondooli@gmail.com>
2020-04-02 06:31:04 +0100

    Initial Commit

---
+++ TheMovieApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheMovieApp
+//
+//  Created by Richard Price on 02/04/2020.
+//  Copyright © 2020 twisted echo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
