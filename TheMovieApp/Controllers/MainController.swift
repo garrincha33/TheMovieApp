3facb3fef52b9418a2b99655e16357ac7c41a128
Richard Price <mrspondooli@gmail.com>
2020-04-06 12:23:08 +0100

    rendering movie title into cell using cell for item

--- TheMovieApp/Controllers/MainController.swift
+++ TheMovieApp/Controllers/MainController.swift
@@ -10,22 +10,24 @@ import UIKit
 
 class MainController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
-    var movies = [Movies]()
+    //step 1 create movie array
+    var movies = [Movie]()
+
     
-    //step 1 create am arrary of items
-    let items = [
-
-        Movies(title: "test", id: 1),
-        Movies(title: "two", id: 2),
-        Movies(title: "three", id: 3),
-        Movies(title: "four", id: 4),
-        Movies(title: "four", id: 4),
-        Movies(title: "four", id: 4),
-        Movies(title: "four", id: 4),
-        Movies(title: "four", id: 4),
-        Movies(title: "four", id: 4)
-
-    ]
+    
+//    let items = [
+//
+//        Movies(title: "test", id: 1),
+//        Movies(title: "two", id: 2),
+//        Movies(title: "three", id: 3),
+//        Movies(title: "four", id: 4),
+//        Movies(title: "four", id: 4),
+//        Movies(title: "four", id: 4),
+//        Movies(title: "four", id: 4),
+//        Movies(title: "four", id: 4),
+//        Movies(title: "four", id: 4)
+//
+//    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,49 +48,60 @@ class MainController: UICollectionViewController, UICollectionViewDelegateFlowLa
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        return items.count
+        return movies.count
         
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        //step 5 make it look more like a cell
+
         return .init(width: view.frame.width, height: 250)
         
     }
 
-    //step 2 change to array of movies
-    func configure<T: ConfigureCell>(_ cellType: T.Type, with app: [Movies], for indexPath: IndexPath) -> T {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellType.reuseIdentifer, for: indexPath) as? T else {
-            fatalError("Unable to dequeue \(cellType)")
-        }
-
-        cell.configure(with: app)
-        return cell
-    }
+    //step 2 comment out for now
+    
+//    func configure<T: ConfigureCell>(_ cellType: T.Type, with app: [Movie], for indexPath: IndexPath) -> T {
+//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellType.reuseIdentifer, for: indexPath) as? T else {
+//            fatalError("Unable to dequeue \(cellType)")
+//
+//        }
+//
+//        cell.configure(with: app)
+//        return cell
+//    }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        return configure(MovieCell.self, with: items, for: indexPath)
+        //step 5 comment out configure to test cell rendering
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MovieCell.reuseIdentifer, for: indexPath) as! MovieCell
+        cell.item = movies[indexPath.row]
+        return cell
+        
+        
+        //return configure(MovieCell.self, with: movies, for: indexPath)
         
     }
-    //step 10 create api service function and call in view did load
+
     private func getMovies() {
         APIService.shared.fetchMovies(completionHandler: { (movie, err) in
             
             if let err = err {
                 print("unable to getch mopvies",err)
             }
-            guard let movies = movie else {return}
-            for m in movies {
-                print("these are you movies \(m.title ?? "")")
+//step 3 add movies array to call
+            guard let moviess = movie else {return}
+            for _ in moviess {
+                self.movies = moviess
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                    }
                 }
             })
         }
     }
     
-    
-    
+
+
 
 
 import SwiftUI
